b7ad9610ee1622300da2e6f389e44b036336999a
KabsMuuka <Kabwalimuuka@gmail.com>
2023-08-29 15:02:26 +0200

    removed some depulicated comments

--- calculator.js
+++ calculator.js
@@ -1,30 +1,23 @@
 const display = document.getElementById('display');
-
+//calculated results will be appended here
 let userInput = '';
-//querySelector grabs all the buttons
-const buttonsContent = document.querySelectorAll('.buttons button');
 
+const buttonsContent = document.querySelectorAll('.buttons button');
 
 buttonsContent.forEach(button =>{
     button.addEventListener('click',()=>{
-        //show the button that is click
-       // console.log("cliked",button.textContent);
-        
-       //Grab the button TextContent and show it in input field
+       //Grab the button TextContent and output it in input field
        display.value = button.textContent;
         switch(button.textContent){
             case '=':
-                //display what the user as cliked
-               // console.log('User input:', userInput);
                 display.value = userInput;
                 //evalution of operands and operators
                 const result = eval(userInput);
-
                 display.value = result;
-                //performing addion
+
+                //clear the users inputs
                 userInput = '';
             break;
-
 
             case 'C':
                 //clear text field if user === C
@@ -33,7 +26,7 @@ buttonsContent.forEach(button =>{
 
             default:
                 userInput += button.textContent;
-                display.value = userInput;
+               // display.value = userInput;
             break;
 
         }
